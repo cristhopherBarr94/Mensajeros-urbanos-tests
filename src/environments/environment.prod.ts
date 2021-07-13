07f93c5239fb6aa20e22f4f3aee49416649f4812
Cristhopher <u1802795@unimilitar.edu.co>
2021-07-13 13:28:01 -0500

    add variables to prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  serverUrl: 'https://restcountries.eu/rest/v2/',
+
+  countries: {
+    getAll: 'all',
+    getByRegion: 'region/', 
+    getByCode: 'alpha/'
+  }
 };
